73ed955f27ea30a27a6434e3f0659f642e18de4e
Riteshsitaula <sitaularitesh12@gmail.com>
2024-03-14 17:00:39 +0545

    update

---
+++ basic.py
@@ -0,0 +1,33 @@
+print("ram")
+print('123')
+name="hari"
+print(name)
+x=5
+y=10
+print(x)
+print(y)
+print(x, y)
+a=1,2,3,
+print(a)
+a,b,*c=56,45,234,56,
+print(a,b,c)
+
+num=3+4
+print(num)
+a=5
+a= a+300
+print(a)
+
+
+fname='ram'
+lname='aaa'
+print(fname,lname)
+
+
+
+for x in range(11):
+    print(x)
+
+num=input("enter a number")
+print(num)
+
