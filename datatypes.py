c97eff6f004019dbd0d49ebaf082b5f326125427
Riteshsitaula <sitaularitesh12@gmail.com>
2024-03-19 16:20:59 +0545

    update

---
+++ datatypes.py
@@ -0,0 +1,66 @@
+
+
+# students=[
+#     {
+#         'name':"ram",
+#         "country":[
+#             {
+#                 'name':"nepal",
+#                 'capital':"ktm"
+#             },
+#             {
+#                 'name':"india",
+#                 'capital':"delhi"
+#             }
+#         ]
+        
+#     },
+#     {
+#         'name':"sita",
+#         "country":[
+#             {
+#                 'name':"nepal",
+#                 'capital':"pokhara"
+#             },
+#             {
+#                 'name':"china",
+#                 'capital':"beijing"
+#             }
+#         ]
+
+#     }
+# ]
+
+# print("my name is", students[0]['name'],"currently living in", students[0]['country'][0]['name'])
+
+
+data=[
+    [
+        {
+            'name':'ram',
+            'age':20
+        },
+        {
+            'name':'sita',
+            'age':21
+        }
+    ],
+    [
+        {
+            'name':'hari',
+            'age':20
+        },
+        {
+            'name':'gita',
+            'age':20
+        }
+    ]
+]
+
+# print(data[0][0]['name'] , data[1][1]['name'])
+
+print(data[0][0]['name'],data[0][0]['age'])
+print(data[0][1]['name'],data[0][1]['age'])
+print(data[1][0]['name'],data[1][0]['age'])
+print(data[1][1]['name'])
+
