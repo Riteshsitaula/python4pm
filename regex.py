359749037a8fe25476eaeb44870713adf9473d55
Riteshsitaula <sitaularitesh12@gmail.com>
2024-04-04 17:04:51 +0545

    update

---
+++ regex.py
@@ -0,0 +1,25 @@
+# course= "PYTHON4PM"
+
+# print(course[6:9])
+
+# import re
+
+# name='1hari'
+
+# #name only contain alphabet
+
+# pattern = re.compile(r'[a-z A-Z]+')
+# if pattern.fullmatch(name):
+#     print('valid')
+# else:
+#     print('invalid')
+
+
+# email='ram@gmail.com'
+# e_pattern= re.compile(r'[a-zA-Z0-9]+@[a-zA-z]+\.[a-zA-z]+')## conditions [a-zA-Z0-9]+@[a-zA-z]+\.[a-zA-z] should match the email written
+# if e_pattern.fullmatch(email):
+#     print('valid email')
+# else:
+#     print('invalid email')
+
+
