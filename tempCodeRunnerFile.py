a688b4ab39f351c5355b38d8505d257ba251867d
Riteshsitaula <sitaularitesh12@gmail.com>
2024-04-25 17:16:10 +0545

    update

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+# try :
+#     print(a)
+# except Exception as e:
+#     print(e)
+# finally:
+#     print ("this is finally blocked")
+# print ("hello world")
+
+# def add(x,y):
+
+#     return x+y
+# try:
+#     print(add(10,20))
+# except Exception as e:
+#     print (e)
+
+
