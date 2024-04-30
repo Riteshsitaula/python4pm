75dd9769398dbe11a90fb8b8568f8bb66f95615c
Riteshsitaula <sitaularitesh12@gmail.com>
2024-04-30 16:34:59 +0545

    update

---
+++ java/custom.js
@@ -0,0 +1,29 @@
+let username="ram";
+// document.getElementById("name").innerHTML="<h1>HEllo " +  username +"</h1>"
+
+// document.getElementById("name").innerHTML=`<h1> hello ${username}</h1>`
+
+// document.getElementById("name").innerText="<h1>HEllo " +  username +"</h1>"
+
+// let userData=[
+//     {name:"Ram", age:"20"},
+//     {name:"shyam", age:"32"},
+//     {name:"hari", age:"35"},
+// ]
+
+// function getUser(){
+//     let output=""
+//     for(let i=0; i<userData.length; i++){
+//         output+=`<h3> <li> ${userData[i].name + userData[i].age} </li></h3>`
+//     }
+//     document.getElementById('userinfo').innerHTML=output
+// }
+// getUser()
+
+// arayfunction
+// call back function
+// arrow function 
+// promise
+// async await
+// oop
+// calss
