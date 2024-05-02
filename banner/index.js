643f0b5003ed1d57ca8d0f11b8f61a01085867ec
Riteshsitaula <sitaularitesh12@gmail.com>
2024-05-02 17:10:21 +0545

    update

---
+++ banner/index.js
@@ -0,0 +1,15 @@
+
+var start=0;
+function imageSlider(){
+    let imageBox = document.getElementsByClassName('image_box');
+    for(let i=0; i<imageBox.length ;i++){
+        imageBox[i].style.display="none";
+    }
+    if(start>imageBox.length-1){
+        start=0;
+    }
+    imageBox[start].style.display="block";
+    setTimeout(imageSlider,2000);
+    start++;
+}
+imageSlider();
